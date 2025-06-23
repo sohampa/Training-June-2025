69bf0e67fa7f04c93b90a5a62facac0448d2c346
pranavjames <111906954+pranavjames@users.noreply.github.com>
2025-06-23 12:05:13 +0530

    Update ProductServiceImpl.java

--- src/main/java/com/example/backend/service/impl/ProductServiceImpl.java
+++ src/main/java/com/example/backend/service/impl/ProductServiceImpl.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Service
 public class ProductServiceImpl implements ProductService {
-
+// Change from feature branch pranav
     @Autowired
     private ProductRepository productRepository;
 
